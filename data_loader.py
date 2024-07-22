dc0be428f4a2b9c4a0ee5977bfcbd885ab1236b6
Smrithi Shera Fernando <55974244+Smrithishera@users.noreply.github.com>
2024-07-22 23:29:22 +0530

    Created using Colab

--- data_loader.py
+++ data_loader.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyNDF3AbSa6Md+smTRIHTHVf",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyNDF3AbSa6Md+smTRIHTHVf"
     },
     "kernelspec": {
       "name": "python3",
@@ -16,16 +15,6 @@
     }
   },
   "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Smrithishera/gan-generated-nlp/blob/main/data_loader.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
     {
       "cell_type": "code",
       "execution_count": 1,
